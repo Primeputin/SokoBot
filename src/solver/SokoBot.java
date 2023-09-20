07bef5033d483f885ca26a63b2121ebacad7ba42
Vladimir <vladimirtang@gmail.com>
2023-09-20 21:54:28 +0800

    record of previous states

--- src/solver/SokoBot.java
+++ src/solver/SokoBot.java
@@ -1,19 +1,18 @@
 package solver;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class SokoBot {
   public String solveSokobanPuzzle(int width, int height, char[][] mapData, char[][] itemsData) {
-    /*
-     * YOU NEED TO REWRITE THE IMPLEMENTATION OF THIS METHOD TO MAKE THE BOT SMARTER
-     */
+
     int x = 0;
     int y = 0;
-    Map<Integer, Integer> boxes = new HashMap<Integer, Integer>();
+    ArrayList<int[]> states = new ArrayList<>();
     Map<Integer, Integer> targets = new HashMap<Integer, Integer>();
+    PriorityQueue<Integer> minCosts = new PriorityQueue<>();
+    ArrayList<Integer> boxes = new ArrayList<>();
 
+    // Finding the position of the player, boxes, and targets
     for (int i = 0; i < width; i++)
     {
       for (int j = 0; j < height; j++)
@@ -25,7 +24,8 @@ public class SokoBot {
         }
         else if (itemsData[j][i] == '$')
         {
-          boxes.put(i, j);
+          boxes.add(i);
+          boxes.add(j);
         }
 
         if (mapData[j][i] == '.')
@@ -35,13 +35,62 @@ public class SokoBot {
 
       }
     }
+    // Adding the starting state to the visited states
+    int[] startState = new int[boxes.size()];
+    startState[0] = x;
+    startState[1] = y;
+    for (int i = 2; i <  boxes.size(); i++)
+    {
+      startState[i] =  boxes.get(i);
+    }
+    states.add(startState);
+
+
     System.out.println("height: " + height + " width: " + width);
     System.out.println("x: " + x + " y: " + y);
     System.out.println(actions(width, height, x, y, mapData, itemsData));
+
+    for (char action: actions(width, height, x, y, mapData, itemsData))
+    {
+      System.out.println(cost(succX(x, action), succY(y, action), boxes));
+    }
     return "lrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlr";
   }
+
+  public static int cost(int x, int y, ArrayList<Integer> boxes)
+  {
+    // if move box out of target, cost = 2
+    // if move box, cost = 1
+    // if just moving, cost = 0
+
+    // heuristic value = minimum manhattan distance between each box and a target
+    return 0;
+  }
+
+
+  public static int succX(int x , char move)
+  {
+      return switch (move) {
+          case 'r' -> x + 1;
+          case 'l' -> x - 1;
+          default -> x;
+      };
+  }
+
+  public static int succY(int y, char move)
+  {
+      return switch (move) {
+          case 'd' -> y + 1;
+          case 'u' -> y - 1;
+          default -> y;
+      };
+  }
+
   public static ArrayList<Character> actions(int width, int height, int x, int y, char[][] mapData, char[][] itemsData)
   {
+
+    // need to implement where it checks if the action will go to a visited state
+
     ArrayList<Character> moves = new ArrayList<>();
     if (mapData[y][x + 1] != '#')
     {
